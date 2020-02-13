33af14d76e8dd89de24e11acf3a8e67901f6e06c
Jantzen-E <jantzen.egan@gmail.com>
2020-02-12 20:04:15 -0700

    fixed db errors and find function

--- app.js
+++ app.js
@@ -25,31 +25,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors);
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/api', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.use('/', indexRouter);
+// app.use('/api', usersRouter);
 
 app.get('/api/waterFilters', cors(), async function(req, res) {
   // const result = Object.values(products);
@@ -74,5 +58,25 @@ app.get('/api/waterFilters/:id', cors(), async function(req, res) {
       res.send('No product with ID: ' + id + ' found!');
   }
 });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+
 
 module.exports = app;
